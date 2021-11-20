717da6e51f12f025a9c9f168fe259b0e1556f7dc
Sarvani <77087769+nsarvani@users.noreply.github.com>
2021-11-20 11:37:36 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,86 @@
+
+var antelopef1, wolf1, bg;
+var antelope, wolf;
+var obstacle, obstacleGroup
+var survivalTime
+
+function preload() 
+{
+
+  bg1 = loadImage("./images/bg.png");
+  antelopef1=loadAnimation("./images/Antelope-1.png","./images/Antelope-2.png","./images/Antelope-3.png","./images/Antelope-4.png")
+  wolf1 = loadAnimation("./images/Wolf-1.png","./images/Wolf-2.png","./images/Wolf-3.png","./images/Wolf-4.png","./images/Wolf-5.png","./images/Wolf-6.png","./images/Wolf-7.png","./images/Wolf-8.png","./images/Wolf-9.png","./images/Wolf-10.png")
+  obstacleImage = loadImage("./images/obstacle.png")
+}
+
+function setup() 
+{
+
+  createCanvas(600,700);
+
+  bg = createSprite(300,350,600,700);
+  bg.addImage(bg1);
+  bg.scale = 2.;
+  bg.velocityY = 1;
+
+  antelope= createSprite(300,100,0,0);
+  antelope.addAnimation("antelopef1", antelopef1);
+
+  wolf = createSprite(305,500, 50, 50);
+  wolf.addAnimation("wolfy", wolf1);
+  wolf.scale = 2;
+
+  obstacleGroup=new Group()
+
+
+}
+
+function obstaclessGroup(){
+  if(frameCount % 100 === 0){
+    obstacle = createSprite(200,200,10,40); 
+    obstacle.velocityY = 2;
+    obstacle.addImage(obstacleImage);
+    obstacle.scale = 0.30
+    obstacleGroup.add(obstacle);
+
+  }
+}
+
+function draw() 
+{
+
+  background(0); 
+
+  obstaclessGroup();
+
+  
+  if(bg.y > 450)
+  {
+    bg.y = 350;
+  }
+
+if (keyDown("up"))
+{
+  wolf.y -= 4;
+}
+if (keyDown("down"))
+{
+  wolf.y -= -4;
+}
+if (keyDown("left"))
+{
+  wolf.x -= 4;
+}
+if (keyDown("right"))
+{
+  wolf.x -= -4;
+}
+
+  drawSprites();
+
+  stroke("black");
+  textSize(18);
+  fill("black");
+  survivalTime=Math.ceil(frameCount/frameRate()) 
+  text("Catch the Anelope before it runs away!: "+ survivalTime, 100,50);
+}
